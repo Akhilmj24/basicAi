f935bcc7d4df655aa70fa53906333883185f8b6c
Akhil Mohan M J <inboxakhil321@gmail.com>
2023-01-01 16:17:21 +0530

    added env

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 const configuration = new Configuration({
   organization: "org-k17Bh8YcTSRdQKTjsPy9gAIb",
-  apiKey: "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",
+  apiKey: process.env.OPENAIKEY_SECRET,
 });
 
 // const response = await openai.listEngines();
 
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 
 app.post("/", async (req, res) => {
   const { message, model } = req.body;
